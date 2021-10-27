b300e3808d9fe968b123c2ead810cdc885d59589
Kayoshi-dev <maxime.jme@gmail.com>
2021-10-27 21:36:41 +0200

    Init Express API

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import routes from './routes/index';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use('/', routes);
+
+app.listen(PORT, (err) => {
+    if (!err) {
+        console.log(`Server is running on ${PORT}`);
+    } else {
+        console.log(err);
+    }
+});
